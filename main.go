19af2cdda0ea40a986d96d587875033ce0f454d5
ClaudioRicardo <claudwiolf@hotmail.com>
2022-11-02 23:26:41 -0300

    recipe data model

--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
 
-import "github.com/gin-conic/gin"
+import (
+	"time"
+
+	"github.com/gin-conic/gin"
+)
+
+type Recipe struct {
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingradients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructiuons"`
+	PublishedAt  time.Time `json:"publishedAt"`
+}
 
 func main() {
 	router := gin.Deafult()
